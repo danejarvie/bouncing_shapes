88458bbd7409c49d3800cb07bf3ec7ec53950ef8
danejarvie <danejarvie@gmail.com>
2016-04-17 15:51:52 +1200

    First commit

---
+++ Notes.java
@@ -0,0 +1,28 @@
+/*
+
+A2.java
+
+Creates and A2 object which is a JFrame with the title Bouncing Application.
+
+Creates the three sub panels:
+	Application Panel
+	Tools Panel
+		Combobox - For shapes
+			
+		Combobox - For paths
+		Height textfield
+		Width textfield
+		Colour selector button
+		
+		
+		
+	Button Panel
+
+
+
+
+
+
+
+
+*/
---
+++ src/MovingSquare.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+public class MovingSquare extends MovingRectangle{
+
+	/** constuctor to create a square with default values
+	 */
+	public MovingSquare() {
+		super();
+	}
+
+	/** constuctor to create a square shape
+	 */
+	public MovingSquare(int x, int y, int w, int h,  int mw, int mh, Color bc, int pathType) {
+		super(x ,y ,w, h ,mw ,mh ,bc, pathType);
+	}
+}
